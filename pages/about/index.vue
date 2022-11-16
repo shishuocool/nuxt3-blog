9d7501935fb3b5979505c536d314ed6c7fde430b
shishuocool <46924247+shishuocool@users.noreply.github.com>
2022-11-16 15:06:14 +0800

    Update index.vue

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -7,11 +7,8 @@ const commitUrl = computed(() => `https://github.com/${config.githubName}/${conf
 const buildTime = ref<string>("Unknown");
 
 const paragraphs = [
-  "幽深宇宙已岁逾百亿，惟闪烁星光点缀生机",
-  "我常仰望浩瀚天际，思念在同一颗星球的你",
-  "想，那转瞬的迷人流星，也许就是你的回眸",
-  "光坠之地，吾之忧祈",
-  "——2021.12.4"
+  "鸟去鸟来山色里，人歌人哭水声中。",
+ 
 ];
 
 onBeforeMount(async () => {
