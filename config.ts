b5047128114f4f6c5be5f398bd285362ce5d244b
shishuocool <46924247+shishuocool@users.noreply.github.com>
2025-09-25 23:33:17 +0800

    Update config.ts

--- config.ts
+++ config.ts
@@ -5,11 +5,11 @@ export default {
 
   /** -------------------------------------------------以下可选修改----------------------------------------------------- */
 
-  title: "yun yu yuan", // 网站标题
-  nickName: "云与原", // 昵称
-  domain: "https://blog.yunyuyuan.net", // rss域名
-  SEO_title: " - yunyuyuan blog", // 搜索引擎显示的标题
-  SEO_keywords: "yunyuyuan,yunyuyuan's blog,云与原,云与原的博客", // keywords meta header
+  title: "shishuo", // 网站标题
+  nickName: "shishuo", // 昵称
+  domain: "https://mmm.st", // rss域名
+  SEO_title: " - shishuo blog", // 搜索引擎显示的标题
+  SEO_keywords: "shishuo,shishuo's blog,石硕,石硕的博客", // keywords meta header
   MSClarityId: "", // Microsoft的Clarity统计，https://clarity.microsoft.com/
   CloudflareAnalyze: "", // cloudflare的统计，https://developers.cloudflare.com/analytics/web-analytics
   CommentRepoId: "", // 评论系统，参考 https://github.com/yunyuyuan/nuxt3-blog/wiki/2.3-%E8%AF%84%E8%AE%BA%E7%B3%BB%E7%BB%9F
@@ -28,11 +28,8 @@ export default {
   themeColorDark: "neutral", // 主题色(夜间模式)
   defaultLang: "zh", // default language, "zh" and "en" are supported currently
   about: [
-    "幽深宇宙已岁逾百亿，惟闪烁星光点缀生机",
-    "我常仰望浩瀚天际，思念在同一颗星球的你",
-    "想，那转瞬的迷人流星，也许就是你的回眸",
-    "光坠之地，吾之忧祈",
-    "——2021.12.4"
+    "让世界充满爱",
+    
   ],
 
   /** -------------------------------------------------注意----------------------------------------------------- */
